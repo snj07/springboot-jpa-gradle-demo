1ffca1c69e11d73490b12badcf4fb947517b1e97
snj07 <sanjaysharma4758@gmail.com>
2018-10-19 23:37:09 +0530

    Update Swagger config for oauth in same page

--- src/main/java/com/snj/SwaggerConfig.java
+++ src/main/java/com/snj/SwaggerConfig.java
@@ -2,86 +2,121 @@ package com.snj;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.*;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hibernate.validator.internal.util.CollectionHelper.newArrayList;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackages = "com.authentication.api")
 public class SwaggerConfig {
 
-    @Value("${CLIENT_ID}")
+    @Value("${config.oauth2.clientID}")
     private String CLIENT_ID;
 
-    @Value("${CLIENT_SECRET}")
+    @Value("${config.oauth2.clientSecret}")
     private String CLIENT_SECRET;
 
-    @Value("${AUTH_SERVER}")
+    @Value("${config.oauth2.accessTokenUri}")
     private String AUTH_SERVER;
 
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("Spring Boot JPA Demo")
-                .description("\"Spring Boot demo project\"")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-                .build();
-    }
+    public static final String securitySchemaOAuth2 = "oauth2schema";
+    public static final String authorizationScopeGlobal = "global";
+    public static final String authorizationScopeGlobalDesc = "accessEverything";
 
+    /**
+     * @return Docket
+     */
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+    public Docket productApi() {
+
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(Arrays.asList(securityScheme()))
-                .securityContexts(Arrays.asList(securityContext()));
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Arrays.asList(securitySchema(), apiKey(), apiCookieKey()))
+                .apiInfo(apiInfo());
+
+
+    }
+
+    @Bean
+    public SecurityScheme apiKey() {
+        return new ApiKey(HttpHeaders.AUTHORIZATION, "apiKey", "header");
+    }
+
+    @Bean
+    public SecurityScheme apiCookieKey() {
+        return new ApiKey(HttpHeaders.COOKIE, "apiKey", "cookie");
+    }
+
+    private OAuth securitySchema() {
+
+        List<AuthorizationScope> authorizationScopeList = newArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = newArrayList();
+        GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant(AUTH_SERVER);
+        grantTypes.add(passwordCredentialsGrant);
+
+        return new OAuth("oauth2", authorizationScopeList, grantTypes);
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .build();
+    }
+
+
+    private List<SecurityReference> defaultAuth() {
+
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+
+        return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
     }
 
     @Bean
     public SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId(CLIENT_ID)
-                .clientSecret(CLIENT_SECRET)
-                .scopeSeparator(" ")
-                .useBasicAuthenticationWithAccessCodeGrant(true)
+        return new SecurityConfiguration
+                ("client", "secret", "", "", "Bearer access token", ApiKeyVehicle.HEADER, HttpHeaders.AUTHORIZATION, "");
+    }
+
+
+    /**
+     * @return ApiInf
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Authentication API").description("")
+                .termsOfServiceUrl("https://www.example.com/api")
+                .contact(new Contact("Developers", "https://projects.spring.io/spring-boot/", ""))
+                .license("Open Source")
+                .licenseUrl("\"https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0.0")
                 .build();
+
     }
 
-    private SecurityScheme securityScheme() {
-        GrantType grantType = new AuthorizationCodeGrantBuilder()
-                .tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/oauth/token", "oauthtoken"))
-                .tokenRequestEndpoint(
-                        new TokenRequestEndpoint(AUTH_SERVER + "/authorize", CLIENT_ID, CLIENT_ID))
-                .build();
-
-        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
-                .grantTypes(Arrays.asList(grantType))
-                .scopes(Arrays.asList(scopes()))
-                .build();
-        return oauth;
-    }
-
-    private AuthorizationScope[] scopes() {
-        AuthorizationScope[] scopes = {
-                new AuthorizationScope("read", "for read operations"),
-                new AuthorizationScope("write", "for write operations"),
-                new AuthorizationScope("foo", "Access foo API")};
-        return scopes;
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(Arrays.asList(
-        new SecurityReference("spring_oauth", scopes()))).forPaths(PathSelectors.regex("/foos.*")).build();
-    }
 
 }
