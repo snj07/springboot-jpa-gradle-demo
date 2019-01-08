b789f9ebedd2a2ca3d8f2d45379554ebb436ce9f
snj07 <sanjaysharma4758@gmail.com>
2019-01-08 22:31:48 +0530

    Add changes for h2 db

--- src/main/java/com/snj/SwaggerConfig.java
+++ src/main/java/com/snj/SwaggerConfig.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import springfox.documentation.builders.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
 import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
@@ -23,21 +24,10 @@ import static org.hibernate.validator.internal.util.CollectionHelper.newArrayLis
 
 @Configuration
 @EnableSwagger2
-@ComponentScan(basePackages = "com.authentication.api")
+@ComponentScan(basePackages = "com.snj")
 public class SwaggerConfig {
 
-    @Value("${config.oauth2.clientID}")
-    private String CLIENT_ID;
 
-    @Value("${config.oauth2.clientSecret}")
-    private String CLIENT_SECRET;
-
-    @Value("${config.oauth2.accessTokenUri}")
-    private String AUTH_SERVER;
-
-    public static final String securitySchemaOAuth2 = "oauth2schema";
-    public static final String authorizationScopeGlobal = "global";
-    public static final String authorizationScopeGlobalDesc = "accessEverything";
 
     /**
      * @return Docket
@@ -51,57 +41,23 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .securityContexts(Collections.singletonList(securityContext()))
-                .securitySchemes(Arrays.asList(securitySchema(), apiKey(), apiCookieKey()))
                 .apiInfo(apiInfo());
 
 
     }
-
-    @Bean
-    public SecurityScheme apiKey() {
-        return new ApiKey(HttpHeaders.AUTHORIZATION, "apiKey", "header");
-    }
-
-    @Bean
-    public SecurityScheme apiCookieKey() {
-        return new ApiKey(HttpHeaders.COOKIE, "apiKey", "cookie");
-    }
-
-    private OAuth securitySchema() {
-
-        List<AuthorizationScope> authorizationScopeList = newArrayList();
-        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
-        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
-
-        List<GrantType> grantTypes = newArrayList();
-        GrantType passwordCredentialsGrant = new ResourceOwnerPasswordCredentialsGrant(AUTH_SERVER);
-        grantTypes.add(passwordCredentialsGrant);
-
-        return new OAuth("oauth2", authorizationScopeList, grantTypes);
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth())
-                .build();
-    }
-
-
-    private List<SecurityReference> defaultAuth() {
-
-        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
-        authorizationScopes[0] = new AuthorizationScope("read", "read all");
-        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
-        authorizationScopes[2] = new AuthorizationScope("write", "write all");
-
-        return Collections.singletonList(new SecurityReference("oauth2", authorizationScopes));
-    }
-
-    @Bean
-    public SecurityConfiguration security() {
-        return new SecurityConfiguration
-                ("client", "secret", "", "", "Bearer access token", ApiKeyVehicle.HEADER, HttpHeaders.AUTHORIZATION, "");
-    }
+//    @Bean
+//    SecurityConfiguration security() {
+//
+//        return new SecurityConfiguration(
+//                "client",
+//                "secret",
+//                "",
+//                "",
+//                "{{X-XSRF-COOKIE}}",
+//                ApiKeyVehicle.HEADER,
+//                "X-XSRF-TOKEN",
+//                "" /*scope separator*/);
+//    }
 
 
     /**
@@ -115,8 +71,6 @@ public class SwaggerConfig {
                 .licenseUrl("\"https://www.apache.org/licenses/LICENSE-2.0")
                 .version("1.0.0")
                 .build();
-
     }
-
 
 }
